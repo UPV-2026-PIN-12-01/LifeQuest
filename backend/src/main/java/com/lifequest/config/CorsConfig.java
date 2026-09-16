6591e328185276c891573a3fbc1f7734bb7e974e
vasyl-ks <vasylks@outlook.es>
2026-09-16 22:55:16 +0200

    #42 refactor(backend): split packages into layered structure

--- backend/src/main/java/com/lifequest/config/CorsConfig.java
+++ backend/src/main/java/com/lifequest/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.lifequest;
+package com.lifequest.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
