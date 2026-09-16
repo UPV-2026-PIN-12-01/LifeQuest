d1b7b32496a4c0eb4dafeed2cf2f0f01d3b8e933
vasyl-ks <vasylks@outlook.es>
2026-09-16 22:55:16 +0200

    PIN-42 refactor(backend): split packages into layered structure

--- backend/src/main/java/com/lifequest/controller/HelloController.java
+++ backend/src/main/java/com/lifequest/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.lifequest;
+package com.lifequest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- backend/src/test/java/com/lifequest/controller/HelloControllerTest.java
+++ backend/src/test/java/com/lifequest/controller/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.lifequest;
+package com.lifequest.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
